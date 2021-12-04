a79cbfe90b2e4895594e1d0c881c065a22742876
Davy M <87863422+iStyyx@users.noreply.github.com>
2021-12-04 14:56:40 +0100

    Adding the final part of Symfony-14

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -45,7 +45,7 @@ class CategoryController extends AbstractController
         // Get data from HTTP request
         $form->handleRequest($request);
         // Was the form submitted?
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             // Get the entity manager
             $entityManager = $this->getDoctrine()->getManager();
             // Persists the category
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,9 +6,12 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ * @UniqueEntity("name")
  */
 class Category
 {
@@ -21,6 +24,8 @@ class Category
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Veuillez donner un nom à votre catégorie")
+     * @Assert\Length(max="255", maxMessage="Le nom de votre catégorie est trop long : {{ limit } caractères maximum.}")
      */
     private $name;
 
